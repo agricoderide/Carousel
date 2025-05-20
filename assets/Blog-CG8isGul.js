f71b47ca7d8e88712580d267a1a631f5b0c0f736
palhinhax <joao.mduart@gmail.com>
2025-05-20 17:16:14 +0100

    Updates

--- assets/Blog-CG8isGul.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Bx7EtR-o.js";function e(){return o.jsxs("div",{children:[o.jsx("h1",{children:"Bem-vindo ao Blog"}),o.jsx("p",{children:"Este é o início."})]})}export{e as default};
---
+++ assets/Home-DT9Jxbs8.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CxUvNrw4.js";function i(){return e.jsxs("div",{children:[e.jsx("h1",{children:"Bem-vindo ao home"}),e.jsx("p",{children:"Este é o início."})]})}export{i as default};
