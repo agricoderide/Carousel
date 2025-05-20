9d7d4d885b3a021d3e98b6fffecb3521c1db0e22
palhinhax <joao.mduart@gmail.com>
2025-05-20 15:42:55 +0100

    fix: update build script in package.json and adjust base path in vite.config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,8 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
 export default function App() {
+  // process.env.NODE_ENV
+  console.log('NODE_ENV', process.env.NODE_ENV)
+
   return <RouterProvider router={router} />
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Carousel/' : '/', // 👈
+  // base: process.env.NODE_ENV === 'production' ? '/Carousel/' : '/',
+  base: '/Carousel/',
 })
