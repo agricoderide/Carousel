26ebf18a027a2e9bea406d02a45789c280b4b148
palhinhax <joao.mduart@gmail.com>
2025-05-20 15:39:25 +0100

    Updates

--- assets/Home-BetXeHpm.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BYulMBso.js";function i(){return e.jsxs("div",{children:[e.jsx("h1",{children:"Bem-vindo ao home"}),e.jsx("p",{children:"Este é o início."})]})}export{i as default};
